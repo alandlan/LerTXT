7e37586e01b713f097155e65fd57513e61b84773
Alan Martins <alanhorcrux@gmail.com>
2019-01-15 20:23:08 -0200

    Push programar ler arquivos txt

---
+++ Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace LerArquivoTxt
+{
+    partial class LerArquivoTXT
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lerButton = new System.Windows.Forms.Button();
+            this.dadosListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lerButton
+            // 
+            this.lerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lerButton.Location = new System.Drawing.Point(12, 12);
+            this.lerButton.Name = "lerButton";
+            this.lerButton.Size = new System.Drawing.Size(75, 23);
+            this.lerButton.TabIndex = 0;
+            this.lerButton.Text = "Ler";
+            this.lerButton.UseVisualStyleBackColor = true;
+            this.lerButton.Click += new System.EventHandler(this.lerButton_Click);
+            // 
+            // dadosListBox
+            // 
+            this.dadosListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dadosListBox.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dadosListBox.FormattingEnabled = true;
+            this.dadosListBox.ItemHeight = 14;
+            this.dadosListBox.Location = new System.Drawing.Point(12, 41);
+            this.dadosListBox.Name = "dadosListBox";
+            this.dadosListBox.Size = new System.Drawing.Size(776, 396);
+            this.dadosListBox.TabIndex = 1;
+            // 
+            // LerArquivoTXT
+            // 
+            this.AcceptButton = this.lerButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dadosListBox);
+            this.Controls.Add(this.lerButton);
+            this.Name = "LerArquivoTXT";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ler Txt";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button lerButton;
+        private System.Windows.Forms.ListBox dadosListBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LerArquivoTxt
+{
+    public partial class LerArquivoTXT : Form
+    {
+        public LerArquivoTXT()
+        {
+            InitializeComponent();
+        }
+
+        private void lerButton_Click(object sender, EventArgs e)
+        {
+
+            dadosListBox.Items.Clear();
+
+            string endereco = @"C:\Dados\ArquivoClientes.txt";
+            try
+            {
+                if (!System.IO.File.Exists(endereco))
+                {
+                    throw new Exception("ArquivoClientes.txt não foi localizado!!");                      
+                }
+
+                string[] dados = new string[4];
+
+                System.IO.StreamReader leitor = new System.IO.StreamReader(endereco, System.Text.Encoding.UTF7);
+
+                // Definir o cabeçalho na ListBox
+                dadosListBox.Items.Add
+                (
+                "CÓDIGO".PadRight(7) +
+                "CLIENTE".PadRight(40) +
+                "CIDADE".PadRight(20) +
+                "PAÍS"
+                );
+
+                dadosListBox.Items.Add(new string('-', 80));
+
+                while (!leitor.EndOfStream)
+                {
+                    dados = leitor.ReadLine().Split(';');
+
+                    dadosListBox.Items.Add
+                        (
+                        dados[0].PadRight(7) +
+                        dados[1].PadRight(40) +
+                        dados[2].PadRight(20) +
+                        dados[3]
+                        );
+                }
+
+                leitor.Close();
+
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Alerta de Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            
+
+        }
+    }
+}
